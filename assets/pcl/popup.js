4ee73b4e8188d7159e25c9edcde94a818a2057a7
Ben Fairbanks <bdfairbanks3@gmail.com>
2017-05-05 13:47:43 -0500

    still need slight content rewrite and new last image

---
+++ assets/pcl/popup.js
@@ -0,0 +1,16 @@
+debugger
+window.addEventListener("load", function() {
+  debugger
+  prime()
+
+});
+
+function prime() {
+  document.getElementsByClass("brag_box").addEventListener('click', popUp);
+}
+
+function popUp(event) {
+  debugger
+  document.getElementById("modal").style.display="block";
+}
+}
---
+++ assets/popup.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", function() {
+  prime()
+
+});
+
+function prime() {
+  prime_pics()
+  document.getElementsByClassName("modal__close")[0].addEventListener('click', close);
+}
+
+function prime_pics(){
+  box = document.getElementsByClassName("brag_box");
+  for (i=0; i<box.length; i++){
+    box[i].addEventListener('click', popUp);
+  }  
+}
+function popUp(event) {
+  document.getElementById("modal").style.display = "block";
+  document.getElementsByClassName("modal__body")[0].innerHTML = "<img src = \"" + event.target.src + "\" class=\"modal__pic\">";
+} 
+
+function close() {
+  document.getElementById("modal").style.display = "none";
+}
